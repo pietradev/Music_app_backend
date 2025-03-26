059f1979c9c0d0de2adb0dde75393260aa2922aa
Pietra Oliveira <pietradev@gmail.com>
2025-03-26 12:32:17 -0400

    feat(models): creating models and making migrations

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Playlist, Track, PlaylistTrackMap, Artist, ArtistTrackMap
 
-# Register your models here.
+admin.site.register(Playlist)
+admin.site.register(Track)
+admin.site.register(PlaylistTrackMap)
+admin.site.register(Artist)
+admin.site.register(ArtistTrackMap)
--- music/models.py
+++ music/models.py
@@ -11,16 +11,39 @@ class Playlist(models.Model):
         return self.name
 
 #Creating Tracks table
-class Tracks(models.Model):
+class Track(models.Model):
     title = models.CharField(max_length=255)
     spotify_id = models.CharField(max_length=100)
     popularity = models.IntegerField()
 
+    def __str__(self):
+        return self.title
+
 
 #Creating playlist-track table
-class PlaylistTrackmap(models.Model):
+class PlaylistTrackMap(models.Model):
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-    track = models.ForeignKey(Tracks, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
 
-
+    class Meta:
+        unique_together = ('playlist', 'track')
     
+    def __str__(self):
+        return f"{self.playlist.name} - {self.track.title}"
+    
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    spotify_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class ArtistTrackMap(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('artist', 'track')
+    
+    def __str__(self):
+        return f"{self.artist.name} - {self.track.title}"
